fb04c2217dc4a7d30480b9c0b8873890931bf5f1
Adam Brill <brill.adam@gmail.com>
2016-05-23 07:28:06 -0700

    dispatch client-side actions

--- src/index.jsx
+++ src/index.jsx
@@ -3,21 +3,19 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
+import {setState} from './action_creators';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch(setState(state))
+);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
@@ -27,6 +25,6 @@ const routes = <Route component={App}>
 ReactDOM.render(
   <Provider store={store}>
     <Router history={hashHistory}>{routes}</Router>
-  </Provider>,,
+  </Provider>,
   document.getElementById('app')
 );
